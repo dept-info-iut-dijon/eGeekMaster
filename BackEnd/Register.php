8137df6ab3e903cc490f2471a50fcf79f22878b0
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-10-03 14:24:13 +0200

    maj backend

---
+++ BackEnd/Register.php
@@ -0,0 +1,18 @@
+<?php
+    public class Register()
+    {
+        $_id;
+        $_login;
+        $_hash;
+        $_firstName;
+        $_lastName;
+        $_email;
+        
+
+        public function __construct(string $login, string $password,)
+        {
+            
+        }
+    }
+
+?>
---
+++ BackEnd/enumGender.php
@@ -0,0 +1,9 @@
+<?php
+
+    class enumGender {
+        const MASCULINE  = "Masculine";
+        const FEMININE = "Feminine";
+        const OTHER = "Other";
+    }
+
+?>
