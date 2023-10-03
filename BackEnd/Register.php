3512adfac608d2184ea4647252556aa6f45c3c67
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-10-03 14:42:45 +0200

    Update Register.php

--- BackEnd/Register.php
+++ BackEnd/Register.php
@@ -7,11 +7,20 @@
         $_firstName;
         $_lastName;
         $_email;
-        
+        $_gender;
+        $_placeFamily;
+        $_dateBirth;
 
-        public function __construct(string $login, string $password,)
+        public function __construct(string $login, string $password, string $firstName, string $lastName, string $email, string $gender, string $placeFamily, string $dateBirth)
         {
-            
+            $this->_login = $login;
+            $this->_firstName = $firstName;
+            $this->_lastName = $lastName;
+            $this->_email = $email;
+            $this->_gender = $gender;
+            $this->_placeFamily = $placeFamily;
+            $this->_dateBirth = $dateBirth;
+            $this->_hash = $password;
         }
     }
 
