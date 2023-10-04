eac115cb1c4ce112fd985571c630ff23025e0ad4
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-10-04 11:57:33 +0200

    Début implémentation inscription vers bdd

--- BackEnd/Process.php
+++ BackEnd/Process.php
@@ -1,6 +1,27 @@
 <?php
-    // Utilisation de la classe avec la fonction setDb
-    $maClasse = new MaClasse();
-    $pdo = new PDO('mysql:host=localhost;dbname=grp-307_s2_prjtut', 'grp-307', 'kmzkmz512');
-    $maClasse->setDb($pdo);
+// Start the session
+session_start();
+
+// Retrieve form data
+$LastName = $_POST['Lastname'];
+$FirstName = $_POST['Firstname'];
+$Email = $_POST['Email'];
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+$BirthDate = $_POST['Year of birth']."-".$_POST['Month of birth']."-".$_POST['Day of birth'];
+$Gender = $_POST['Gender'];
+$FamilyPlace = $_POST['Place family'];
+
+// Store the data in the session
+$_SESSION['LastName'] = $LastName;
+$_SESSION['FirstName'] = $FirstName;
+$_SESSION['Email'] = $Email;
+$_SESSION['Username'] = $Username;
+$_SESSION['Password'] = $Password;
+$_SESSION['BirthDate'] = $BirthDate;
+$_SESSION['Gender'] = $Gender;
+$_SESSION['FamilyPlace'] = $FamilyPlace;
+
+// Redirect the user to another page or perform other operations
+header('Location: next_page.php');
 ?>
