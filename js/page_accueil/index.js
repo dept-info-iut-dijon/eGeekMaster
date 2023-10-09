51f78ad2e8898f59e7f787d4b3dd05ccbb422c21
TheoCornuC1 <133879832+TheoCornuC1@users.noreply.github.com>
2023-10-09 17:25:43 +0200

    Commentaire
    Ajout de commentaire dans plusieurs classes et optimisation du code

--- js/page_accueil/index.js
+++ js/page_accueil/index.js
@@ -1,129 +1,139 @@
-// JavaScript source code
-
-var id = undefined;
-var num_image = 0;
-var currentImage;
-var idSmiley = undefined;
-let isSmiley2Hovered = false;
-var heartInterval = undefined;
-var smiley3Div = undefined;
-
 function main() {
     console.log("Hello, world");
-    listecontrol = document.querySelectorAll(".slider-control");
-    for (let i = 0; i < listecontrol.length; i++) {
-        listecontrol[i].onclick = click;
+
+    // Select all slider controls
+    const sliderControls = document.querySelectorAll(".slider-control");
+
+    // Select smiley elements
+    const smiley1 = document.getElementById("smiley1");
+    const smiley2 = document.getElementById("smiley2");
+    const smiley3 = document.getElementById("smiley3");
+
+    // Select the container for generated hearts
+    const smiley3Div = document.getElementById("smiley3Div");
+
+    // Initialize local variables to manage state
+    let currentImage = 0;
+    let id = setInterval(nextImage, 2000);
+    let isSmiley2Hovered = false;
+    let heartInterval = null;
+    let idSmiley = null;
+
+    // Function to add a CSS class to a set of elements
+    function addClassToElements(elements, className) {
+        elements.forEach((element) => element.classList.add(className));
     }
-    currentImage = 0;
-    id = setInterval(nextImage, 2000);
 
-    // Récupérez les éléments img par leur id
-    var smiley1 = document.getElementById("smiley1");
-    var smiley2 = document.getElementById("smiley2");
-    var smiley3 = document.getElementById("smiley3");
+    // Function to remove a CSS class from a set of elements
+    function removeClassFromElements(elements, className) {
+        elements.forEach((element) => element.classList.remove(className));
+    }
 
-    // Ajoutez un gestionnaire d'événement pour le survol de la souris sur smiley1
-    smiley1.addEventListener("mouseover", () => {
-        smiley1.style.transition = "transform 0.5s"; // Ajoutez une transition de 0.5 seconde
-        smiley1.style.transformOrigin = "center center"; // Définissez l'origine de la transformation au centre de l'image
-        smiley1.style.transform = "rotate(360deg)"; // Applique une rotation de 360 degrés
-    });
+    // Handle rotation of smiley1 on mouseover
+    function rotateSmiley1() {
+        smiley1.style.transition = "transform 0.5s";
+        smiley1.style.transformOrigin = "center center";
+        smiley1.style.transform = "rotate(360deg)";
+    }
 
-    // Ajoutez un gestionnaire d'événement pour le départ de la souris sur smiley1
-    smiley1.addEventListener("mouseout", () => {
-        smiley1.style.transition = "transform 0.5s"; // Ajoutez une transition de 0.5 seconde
-        smiley1.style.transform = "rotate(0deg)"; // Réinitialise la rotation
-    });
+    // Reset rotation of smiley1 when mouse leaves
+    function resetSmiley1() {
+        smiley1.style.transition = "transform 0.5s";
+        smiley1.style.transform = "rotate(0deg)";
+    }
 
-    // Ajoutez un gestionnaire d'événement pour le survol de la souris sur smiley2
-    smiley2.addEventListener("mouseover", () => {
-        isSmiley2Hovered = true; // La souris survole smiley2
-        smiley2.style.transition = "transform 0.5s, scale 0.1s"; // Ajoutez une transition pour la transformation et l'échelle de 0.5 seconde
-        smiley2.style.transformOrigin = "center center"; // Définissez l'origine de la transformation au centre de l'image
-        smiley2.style.transform = "scale(3)"; // Applique une échelle de 3 fois la taille d'origine
-        // Ajoutez une animation de tremblement en utilisant les animations CSS
+    // Handle scaling and trembling of smiley2 on mouseover
+    function scaleAndTrembleSmiley2() {
+        isSmiley2Hovered = true;
+        smiley2.style.transition = "transform 0.5s, scale 0.1s";
+        smiley2.style.transformOrigin = "center center";
+        smiley2.style.transform = "scale(3)";
         tremble();
-    });
+    }
 
-    // Ajoutez un gestionnaire d'événement pour le départ de la souris sur smiley2
-    smiley2.addEventListener("mouseout", () => {
-        isSmiley2Hovered = false; // La souris ne survole plus smiley2
-        smiley2.style.transition = "transform 0.5s, scale 0.2s"; // Ajoutez une transition pour la transformation et l'échelle de 0.5 seconde
-        smiley2.style.transformOrigin = "center center"; // Définissez l'origine de la transformation au centre de l'image
-        smiley2.style.transform = "scale(1)"; // Réinitialise l'échelle à la taille d'origine
-        // Réinitialise l'animation de tremblement
+    // Reset scaling of smiley2 when mouse leaves
+    function resetSmiley2() {
+        isSmiley2Hovered = false;
+        smiley2.style.transition = "transform 0.5s, scale 0.2s";
+        smiley2.style.transformOrigin = "center center";
+        smiley2.style.transform = "scale(1)";
         cancelAnimationFrame(idSmiley);
-    });
+    }
 
-    // Ajoutez un gestionnaire d'événement pour le survol de la souris sur smiley3
-    smiley3.addEventListener("mouseover", () => {
-        // Créez un intervalle pour générer des cœurs à intervalles réguliers
-        heartInterval = setInterval(createHeart, 400); // Génère un cœur toutes les 200 millisecondes (ajustez selon vos besoins)
-       
-    });
-    smiley3.addEventListener("mouseout", () => {
-        // Arrêtez de générer des cœurs lorsque la souris quitte le smiley3
-        clearInterval(heartInterval);
-    });
-    // Ajoutez des gestionnaires d'événements pour les liens du header
-    
-}
-
+    // Create a heart inside smiley3Div
     function createHeart() {
         console.log(smiley3);
-        var smiley3Div = document.getElementById("smiley3Div")
-        var heart = document.createElement("div"); 
+        const heart = document.createElement("div");
         heart.classList.add("heart");
         heart.style.marginLeft = `${Math.random() * 80}%`;
         smiley3Div.appendChild(heart);
         setTimeout(() => {
             heart.remove();
-        },3000);
-        
+        }, 3000);
     }
-    
+
+    // Show the next image in the slider
     function nextImage() {
-        currentImage = (currentImage + 1) % listecontrol.length;
+        currentImage = (currentImage + 1) % sliderControls.length;
         print(currentImage);
     }
-    
+
+    // Make smiley2 tremble
     function tremble() {
         if (isSmiley2Hovered) {
             smiley2.style.transform = `translate(${getRandomOffset()}px, ${getRandomOffset()}px) scale(2)`;
-            idSmiley = requestAnimationFrame(tremble); // Demande une nouvelle frame d'animation
+            idSmiley = requestAnimationFrame(tremble);
         }
     }
-    
+
+    // Get a random offset between -50 and 50
     function getRandomOffset() {
         return (Math.random() * 100) - 50;
     }
-    
-    /**
-     * Active une image du slider
-     * @param {number} number - Le numéro de l'image à activer
-     */
+
+    // Activate a specific image in the slider
     function print(number) {
-        let listeimage = document.querySelectorAll(".slider-panel");
-        let listecontrol = document.querySelectorAll(".slider-control");
-        console.log(listecontrol);
-        for (let i = 0; i < listeimage.length; i++) {
-            listeimage[i].classList.remove("active");
-            listecontrol[i].classList.remove("active");
-        }
-        listeimage[number].classList.add("active");
-        listecontrol[number].classList.add("active");
+        const sliderPanels = document.querySelectorAll(".slider-panel");
+        const sliderControls = document.querySelectorAll(".slider-control");
+
+        removeClassFromElements(sliderPanels, "active");
+        removeClassFromElements(sliderControls, "active");
+
+        sliderPanels[number].classList.add("active");
+        sliderControls[number].classList.add("active");
     }
-    
+
+    // Handle click on a slider control
     function click(evt) {
-        let button = evt.target;
-        let controls = document.querySelectorAll(".slider-control");
-        let pos = [].indexOf.call(controls, button);
+        const button = evt.target;
+        const pos = [...sliderControls].indexOf(button);
         print(pos);
         currentImage = pos;
         clearInterval(id);
         id = setInterval(nextImage, 2000);
     }
-// Créez une fonction pour générer un cœur
 
+    // Add event handlers for actions on smiley1
+    smiley1.addEventListener("mouseover", rotateSmiley1);
+    smiley1.addEventListener("mouseout", resetSmiley1);
 
+    // Add event handlers for actions on smiley2
+    smiley2.addEventListener("mouseover", scaleAndTrembleSmiley2);
+    smiley2.addEventListener("mouseout", resetSmiley2);
+
+    // Add event handlers for actions on smiley3
+    smiley3.addEventListener("mouseover", () => {
+        heartInterval = setInterval(createHeart, 400);
+    });
+    smiley3.addEventListener("mouseout", () => {
+        clearInterval(heartInterval);
+    });
+
+    // Add event handlers for click on slider controls
+    sliderControls.forEach((control) => {
+        control.addEventListener("click", click);
+    });
+}
+
+// Execute the main function when the page is fully loaded
 window.addEventListener('load', main);
