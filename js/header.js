c6e8819b8c76e41434761e053764a955f4a900eb
TheoCornuC1 <133879832+TheoCornuC1@users.noreply.github.com>
2023-10-09 14:24:20 +0200

    header
    ajout du flou pour la barre lateralle

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 function BarreLateral () {
     const menuButton = document.getElementById("bars");
     const menu = document.getElementById("menu");
+    const menuF = document.getElementById("menuF");
 
     if (menu.style.left === "0px") {
         menuButton.style.left = "250px";
@@ -12,9 +13,11 @@ function BarreLateral () {
     menuButton.addEventListener("click", function () {
         if (menu.style.left === "0px") {
             menu.style.left = "-250px";
+            menuF.style.left = "-800vw";
             menu.style["box-shadow"] = "0px 0px 0px black";
         } else {
             menu.style.left = "0px";
+            menuF.style.left = "0px";
             menu.style["box-shadow"] = "0px 0px 30px black";
         }
     });
@@ -22,19 +25,23 @@ function BarreLateral () {
     document.addEventListener("click", function (event) {
         if (event.target !== menuButton && event.target !== menu) {
             menu.style.left = "-250px";
+            menuF.style.left = "-800vw";
             menu.style["box-shadow"] = "0px 0px 0px black";
         }
     });
     document.addEventListener('DOMContentLoaded', function () {
         const menuIcon = document.getElementById('bars');
         const sideMenu = document.getElementById('menu');
+        const sideFlou = document.getElementById('menuF');
     
         menuIcon.addEventListener('click', function () {
             if (sideMenu.style.left === '-250px') {
                 sideMenu.style.left = '0';
+                sideFlou.style.left = '0';
                 menu.style["box-shadow"] = "0px 0px 0px black";
             } else {
                 sideMenu.style.left = '-250px';
+                sideFlou.style.left = '-800vw'
                 menu.style["box-shadow"] = "0px 0px 30px black";
             }
         });
