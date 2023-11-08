47ae0d116bd14290e7a122602427c94b79168737
Lola Cohidon <116801009+LoLuX22@users.noreply.github.com>
2023-11-08 16:54:42 +0100

    Html Référentiel
    Réalisation page HTML du référentiel en ajoutant la barre de recherche ainsi que le tableau du réfrérentiel

--- GoodStructure/Views/viewReference.php
+++ GoodStructure/Views/viewReference.php
@@ -1,7 +1,104 @@
 <!-- author: Théo Cornu
 author: Théo Cornu
-author : Lola-->
+author : Lola Cohidon-->
 
+<div id = "ContaintReference">
 <link rel="stylesheet" href="Public/css/reference.css">
+<!--Search bar-->
+    <div class="InputContainer">
+        <input type="text" name="text" class="input" id="input" placeholder="Search">
+        <label for="input" class="labelforsearch">
+    <svg viewBox="0 0 512 512" class="searchIcon"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
+        </label>
+    <div class="border"></div>
+        <button class="micButton"><svg viewBox="0 0 384 512" class="micIcon"><path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path></svg>
+        </button>
+    </div>
+<!--Reference table-->
+    <div class="ReferenceTable">
+        <table>
+            <tr>
+                <th>Activity</th>
+                <th>Descriptive</th>
+                <th>Value 1/4</th>
+            </tr>
+            <tr>
+                <td>Cleaning</td>
+                <td>Cleaning surfaces, floors, sinks, toilets and household appliances.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Shopping</td>
+                <td>Purchase of all supplies necessary for the household (food, medications, furniture, DIY and garden equipment, children's clothing, etc.).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Cooking</td>
+                <td>Creation of weekly menus, Meal preparation, Table setting and clearing.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Dishes</td>
+                <td>By hand or using the dishwasher (loading and unloading). Storage in cupboards.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Laundry</td>
+                <td>Collecting (putting in the basket), sorting, washing, drying, ironing, and storing.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>ChildrensCare</td>
+                <td>Physiological needs (getting up, washing, feeding, dressing, caring, and putting to bed).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>ChildsPlay</td>
+                <td>Organization (choosing the game, setting up), participation in activities (starting the game, overall participation, and cleaning up the game).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>ChildrensJourney</td>
+                <td>To school, after-school care (recreation center + cafeteria), babysitter, friends, doctor, leisure activities (sports/culture).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>ParentJourney</td>
+                <td>For dependent parent.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>ParentCare</td>
+                <td>Assistance, care for a dependent parent, administrative management of files (health, home care, etc.), transportation (health, leisure).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Administrative</td>
+                <td>Taxes, bills, administrative registrations, health insurance management, bank account management, subscriptions, mail posting, and making reservations.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>PetCare</td>
+                <td>Nourriture, Lavage/brossage, promenade, changement litière, RDV véto, soigner.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Gardening</td>
+                <td>Toute tâche extérieure nécessaire : tondre, élaguer, traiter, arroser, récolter.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>DIY</td>
+                <td>Repairs, necessary minor work (plaster, painting, plumbing, etc.).</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>Household Management</td>
+                <td>Service provider management (search, hiring, follow-up), availability management for parcel reception, leisure management.</td>
+                <td>1</td>
+            </tr>
+        </table>
+    </div>
+</div>
 
 
