982ba8654ac7cb5c92412702a22f7d27b2532557
TheoDeguin <105062188+TheoDeguin@users.noreply.github.com>
2023-11-08 10:41:17 +0100

    [dashboard.css, viewDashboard.php], (bouton tache, suppression menu déroulant)

--- GoodStructure/Views/viewDashBoard.php
+++ GoodStructure/Views/viewDashBoard.php
@@ -1,17 +1,7 @@
 <link rel="stylesheet" href="Public/css/dashboard.css">
     <div id="divChoixMembre">
-        <!-- Selection membre famille -->
-        <form action="post">
-            <label for="memberFamily">Choose a member of the family : </label>
-            <select name="memberFamily" id="memberFamily">
-                <option value="member1">Family member n°1</option>
-                <option value="member2">Family member n°2</option>
-                <option value="member3">Family member n°3</option>
-                <option value="member4">Family member n°4</option>
-                <option value="member5">Family member n°5</option>
-            </select>
-        </form>
-        <button type="button">Register a new task</button>
+        <!-- Bouton d'ajout de tâches -->
+        <button id="boutonTaches" type="button">Register a new task</button>
     </div>
 
     <!-- Titre des graphiques -->
