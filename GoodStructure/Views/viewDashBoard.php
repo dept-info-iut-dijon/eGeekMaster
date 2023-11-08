85b3572cefef16c48f485dccf2bda04c0696eefb
TheoDeguin <105062188+TheoDeguin@users.noreply.github.com>
2023-11-08 15:53:14 +0100

    Modif dashboard

--- GoodStructure/Views/viewDashBoard.php
+++ GoodStructure/Views/viewDashBoard.php
@@ -1,19 +1,47 @@
 <link rel="stylesheet" href="Public/css/dashboard.css">
-<div id="divBoutonTache">
-    <!-- Bouton d'ajout de tâches -->
-    <button id="boutonTaches" type="button">Register a new task</button>
-    <div>
-        <!-- Pour enzo -->
+<div id="body">
+    <div id="divBoutonTache">
+        <!-- Bouton d'ajout de tâches -->
+        <button id="boutonTaches" type="button">Register a new task</button>
+        <div>
+            <!-- Pour enzo -->
+        </div>
+    </div>
+
+    <!-- Titre des graphiques -->
+    <div id="titreDashboard">
+        <div id="piechart">
+            <h1>Tasks distribution</h1>
+            <canvas id="myChart"></canvas>
+        </div>
+        <div id="separator"></div>
+        <div id="barchart">
+            <h1>Average duration for each task</h1>
+        </div>
     </div>
 </div>
 
-<!-- Titre des graphiques -->
-<div id="titreDashboard">
-    <div id="piechart">
-        <h1>Tasks distribution</h1>
-    </div>
-    <div id="separator"></div>
-    <div id="barchart">
-        <h1>Average duration for each task</h1>
-    </div>
-</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'prout'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3, 50],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
