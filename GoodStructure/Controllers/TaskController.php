b616a041d328cdd7753ed59852507d3c59cadc46
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-09 12:32:56 +0100

    Fichier:"TaskController.php" - Class:"TaskController" - Méthode:"__construct()","addTask()"

---
+++ GoodStructure/Controllers/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'Controllers/Router/Route.php';
+
+/**
+ * Class RouteTaskRegistration
+ * @package Controllers\Router\Route
+ * @author Nicolas
+ */
+
+ class TaskController
+ {
+    public function __construct()
+    {
+
+    }
+
+    public function addTask()
+    {
+
+    }
+ }
