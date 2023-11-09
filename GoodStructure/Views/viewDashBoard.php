792a1f24d1e42785e1aa37261f02e64f8336a4d5
TheoDeguin <105062188+TheoDeguin@users.noreply.github.com>
2023-11-09 11:40:50 +0100

    Mise en forme page dashboard

--- GoodStructure/Views/viewDashBoard.php
+++ GoodStructure/Views/viewDashBoard.php
@@ -1,30 +1,34 @@
-<link rel="stylesheet" href="Public/css/dashboard.css">
+<link rel="stylesheet" href="Public/css/dashboard1.css">
 <div id="body">
-        <div id="divBoutonTache">
-                <!-- Bouton d'ajout de tâches -->
-                <button id="boutonTaches" type="button">Register a new task</button>
-                <div>
-                        <!-- Pour enzo -->
-                </div>
-        </div>
+    <div id="divBoutonTache">
+            <!-- Bouton d'ajout de tâches -->
+            <button id="boutonTaches" type="button">Register a new task</button>
+            <div>
+                    <!-- Pour enzo -->
+            </div>
+    </div>
 
-        <!-- Graphiques -->
-        <div id="titreDashboard">
-            <div id="piechart">
-                <h1>Tasks distribution</h1>
-                <canvas id="cv"></canvas>
-            </div>
-            <div id="barchart">
-                <h1>Average duration for each task</h1>
-                <canvas id="coucou"></canvas>
+    <!-- Graphiques -->
+    <div id="titreDashboard">
+        <div id="piechart">
+            <h1>Tasks distribution</h1>
+            <div style="position: relative; height: 380px">
+                <canvas id="myChart1"></canvas>
             </div>
         </div>
+        <div id="barchart">
+            <h1>Average duration for each task</h1>
+            <div style="position: relative">
+                <canvas id="myChart2"></canvas>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-    const ctx1 = document.getElementById('cv');
+    const ctx1 = document.getElementById('myChart1');
 
     new Chart(ctx1, {
         type: 'pie',
@@ -37,11 +41,12 @@
             }]
         },
         options: {
-            
+            responsive: true,
+            maintainAspectRatio: false
         }
     });
 
-    const ctx2 = document.getElementById('coucou');
+    const ctx2 = document.getElementById('myChart2');
 
     new Chart(ctx2, {
         type: 'bar',
