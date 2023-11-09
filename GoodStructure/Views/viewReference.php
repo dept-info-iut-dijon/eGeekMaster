db29338b5329f583868424f26a44a15b317859e8
Lola Cohidon <116801009+LoLuX22@users.noreply.github.com>
2023-11-09 16:19:35 +0100

    changement sur le HTML et CSS de reference
    Modification faites pour que les icones soient à gauche de chaque activités.

--- GoodStructure/Views/viewReference.php
+++ GoodStructure/Views/viewReference.php
@@ -1,21 +1,24 @@
 <!-- author: Théo Cornu
 author: Théo Cornu
 author : Lola Cohidon-->
-
-<div id = "ContaintReference">
-<link rel="stylesheet" href="Public/css/reference1.css">
-
-<!--Search bar-->
+<div id="ContaintReference">
+    <link rel="stylesheet" href="Public/css/reference1.css">
+    <!--Search bar-->
     <div class="InputContainer">
         <input type="text" name="text" class="input" id="input" placeholder="Search">
         <label for="input" class="labelforsearch">
-    <svg viewBox="0 0 512 512" class="searchIcon"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path></svg>
+            <svg viewBox="0 0 512 512" class="searchIcon">
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
+            </svg>
         </label>
-    <div class="border"></div>
-        <button class="micButton"><svg viewBox="0 0 384 512" class="micIcon"><path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path></svg>
+        <div class="border"></div>
+        <button class="micButton">
+            <svg viewBox="0 0 384 512" class="micIcon">
+                <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path>
+            </svg>
         </button>
     </div>
-<!--Reference table-->
+    <!--Reference table-->
     <div class="ReferenceTable">
         <table>
             <tr>
@@ -24,142 +27,215 @@ author : Lola Cohidon-->
                 <th>Value 1/4</th>
             </tr>
             <tr>
-                <td>Cleaning
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_cleaning.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_cleaning.png" alt="">
+                        </div>
+                        <div>
+                            <p>Cleaning</p>
+                        </div>
                     </div>
                 </td>
                 <td>Cleaning surfaces, floors, sinks, toilets and household appliances.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Shopping
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_shopping.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_shopping.png" alt="">
+                        </div>
+                        <div>
+                            <p>Shopping</p>
+                        </div>
                     </div>
                 </td>
                 <td>Purchase of all supplies necessary for the household (food, medications, furniture, DIY and garden equipment, children's clothing, etc.).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Cooking
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_cooking.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_cooking.png" alt="">
+                        </div>
+                        <div>
+                            <p>Cooking</p>
+                        </div>
                     </div>
                 </td>
                 <td>Creation of weekly menus, Meal preparation, Table setting and clearing.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Dishes
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_dishes.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_dishes.png" alt="">
+                        </div>
+                        <div>
+                            <p>Dishes</p>
+                        </div>
                     </div>
                 </td>
                 <td>By hand or using the dishwasher (loading and unloading). Storage in cupboards.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Laundry
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_laundry.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_laundry.png" alt="">
+                        </div>
+                        <div>
+                            <p>Laundry</p>
+                        </div>
                     </div>
                 </td>
                 <td>Collecting (putting in the basket), sorting, washing, drying, ironing, and storing.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>ChildrensCare
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_childrenCare.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_childrenCare.png" alt="">
+                        </div>
+                        <div>
+                            <p>ChildrensCare</p>
+                        </div>
                     </div>
                 </td>
                 <td>Physiological needs (getting up, washing, feeding, dressing, caring, and putting to bed).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>ChildsPlay
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_childsPlay.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_childsPlay.png" alt="">
+                        </div>
+                        <div>
+                            <p>ChildsPlay</p>
+                        </div>
                     </div>
                 </td>
                 <td>Organization (choosing the game, setting up), participation in activities (starting the game, overall participation, and cleaning up the game).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>ChildrensJourney
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_childrensJourney.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_childrensJourney.png" alt="">
+                        </div>
+                        <div>
+                            <p>ChildrensJourney</p>
+                        </div>
                     </div>
                 </td>
                 <td>To school, after-school care (recreation center + cafeteria), babysitter, friends, doctor, leisure activities (sports/culture).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>ParentJourney
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_parentJourney.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_parentJourney.png" alt="">
+                        </div>
+                        <div>
+                            <p>ParentJourney</p>
+                        </div>
                     </div>
                 </td>
                 <td>For dependent parent.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>ParentCare
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_parentCare.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_parentCare.png" alt="">
+                        </div>
+                        <div>
+                            <p>ParentCare</p>
+                        </div>
                     </div>
                 </td>
                 <td>Assistance, care for a dependent parent, administrative management of files (health, home care, etc.), transportation (health, leisure).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Administrative
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_administrative.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_administrative.png" alt="">
+                        </div>
+                        <div>
+                            <p>Administrative</p>
+                        </div>
                     </div>
                 </td>
                 <td>Taxes, bills, administrative registrations, health insurance management, bank account management, subscriptions, mail posting, and making reservations.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>PetCare
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_petCare.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_petCare.png" alt="">
+                        </div>
+                        <div>
+                            <p>PetCare</p>
+                        </div>
                     </div>
                 </td>
                 <td>Food, Washing/Brushing, Walk, Litter change, Vet appointment, Care for.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Gardening
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_gardening.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_gardening.png" alt="">
+                        </div>
+                        <div>
+                            <p>Gardening</p>
+                        </div>
                     </div>
                 </td>
                 <td>Any necessary outdoor tasks: mow, prune, treat, water, harvest.</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>DIY
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_diy.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_diy.png" alt="">
+                        </div>
+                        <div>
+                            <p>DIY</p>
+                        </div>
                     </div>
                 </td>
                 <td>Repairs, necessary minor work (plaster, painting, plumbing, etc.).</td>
                 <td>1</td>
             </tr>
             <tr>
-                <td>Household Management
-                    <div>
-                        <img class="icon" class="action" src="Public/image/page_reference/img_householdManagement.png" alt="">
+                <td>
+                    <div class="action">
+                        <div>
+                            <img class="icon" src="Public/image/page_reference/img_householdManagement.png" alt="">
+                        </div>
+                        <div>
+                            <p>Household Management</p>
+                        </div>
                     </div>
                 </td>
-                <td>Service provider management (search, hiring, follow-up), availability management for parcel reception, leisure management</td>
+                <td>Service provider management (search, hiring, follow-up), availability management for parcel reception, leisure management.</td>
                 <td>1</td>
             </tr>
         </table>
     </div>
 </div>
-
-
