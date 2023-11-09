83fdcaa7718c5e8dd1e7d6b61cb017ef16cab4ea
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-09 12:31:33 +0100

    Update TaskManager.php

---
+++ GoodStructure/Models/TaskManager.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'Model.php';
+require_once 'Task.php';
+
+/**
+ * Class TaskManager
+ * 
+ * @author Nicolas
+ */
+
+ class TaskManager extends Model
+ {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function addTask(Task $task)
+    {
+        
+    }
+ }
