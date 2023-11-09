e6efd04ed02fb95727233539f8708d5d6590349b
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-11-09 22:17:25 +0100

    Fichier:"TaskController.php" - Class:"TaskController" - Méthode:"__construct()"

--- GoodStructure/Controllers/TaskController.php
+++ GoodStructure/Controllers/TaskController.php
@@ -1,21 +1,27 @@
 <?php
-require_once 'Controllers/Router/Route.php';
+require_once 'views/View.php';
+require_once 'models/TaskManager.php';
+require_once 'Controllers/MainController.php';
 
 /**
  * Class RouteTaskRegistration
- * @package Controllers\Router\Route
+ * @package Controllers
  * @author Nicolas
  */
 
- class TaskController
- {
-    public function __construct()
-    {
+class TaskController
+{
+   /**
+    * TaskController constructor.
+    */
+   public function __construct()
+   {
+      $this->TaskManager = new TaskManager();
+      $this->mainController = new MainController();
+   }
 
-    }
-
-    public function addTask()
-    {
-
-    }
- }
+   public function addTask()
+   {
+      
+   }
+}
