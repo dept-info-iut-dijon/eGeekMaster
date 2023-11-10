5be1ea81cb557717a6eded7a947558e4df11f215
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-11-10 01:13:36 +0100

    Fichier:"TaskManager.php" - Class:"TaskManager" - Méthode:"GetAllByDashBoard()","GetById()","AddTask()"

--- GoodStructure/Models/TaskManager.php
+++ GoodStructure/Models/TaskManager.php
@@ -1,11 +1,21 @@
 <?php
 require_once 'Model.php';
 require_once 'Task.php';
+require_once 'DashBoard.php';
+require_once 'Controllers/MainController.php';
+require_once 'Models/DashboardManager.php';
+
+/**
+ * Class DashBoardManager
+ * @package GoodStructure\Models
+ * @author Nicolas
+ * @author Théo Cornu
+ */
 
 /**
  * Class TaskManager
  * 
- * @author Nicolas
+
  */
 
  class TaskManager extends Model
@@ -15,8 +25,90 @@ require_once 'Task.php';
         parent::__construct();
     }
 
-    public function addTask(Task $task)
+    /**
+     * Retrieve a list of Tasks by its dashboard from the database.
+     *
+     * @param DashBoard $dashboards The dashboard to retrieve the tasks from.
+     * @return array An array of Task objects.
+     * @author Théo Cornu
+     */
+    public function GetAllByDashBoard(DashBoard $dashboards): array
     {
-        
+        try {
+            $sql = 'SELECT * FROM task WHERE dashboard = ?';
+            $Tasks = [];
+            $result = $this->executerRequete($sql, [$dashboards->getUsername()]);
+            while ($line = $result->fetch(PDO::FETCH_ASSOC)) {
+                $Task = new Task(
+                    $line['idTask'],
+                    $line['Name'],
+                    $line['Duration'],
+                    $line['Date'],
+                    $line['DashBoardidDashboard'],
+                    
+                );
+
+                $Tasks[] = $Task;
+            }
+            return $Tasks;
+        } catch (PDOException $e) {
+            // In case of an error, redirect to the error page with a message
+            $errorMessage = "An error occurred while retrieving data.";
+            header("Location: index.php?action=Index&errorMessage=".urlencode($errorMessage));
+            exit();
+        }
+    }
+
+    /**
+     * Retrieve a specific Task by its ID from the database.
+     *
+     * @param int $id The ID of the Task to retrieve.
+     * @return Task|null The Task object, or null if not found.
+     * @throws Exception
+     */
+    public function GetByID(int $id): ?Task
+    {
+        try {
+            $sql = 'SELECT * FROM task WHERE idTask = ?';
+            $result = $this->executerRequete($sql, [$id]);
+            $line = $result->fetch(PDO::FETCH_ASSOC);
+            $Task = new Task(
+                $line['idTask'],
+                $line['Name'],
+                $line['Duration'],
+                $line['Date'],
+                $line['DashBoardidDashboard'],
+                
+            );
+            return $Task;
+        } catch (PDOException $e) {
+            // In case of an error, redirect to the error page with a message
+            $errorMessage = "An error occurred while retrieving data.";
+            header("Location: index.php?action=Index&errorMessage=".urlencode($errorMessage));
+            exit();
+        }
+    }
+
+    /**
+     * Add a new Task to the database.
+     *
+     * @param Task $Task The Task to add.
+     * @return Task The Task object, or null if not found.
+     * @throws Exception
+     */
+    public function AddTask(Task $Task): Task
+    {
+        try {
+            $dashboardManager = new DashboardManager();
+            $sql = 'INSERT INTO task (Name, Duration, Date, DashBoardidDashboard) VALUES (?, ?, ?, ?)';
+            $this->executerRequete($sql, [$Task->getNameTask(), $Task->getDuration(), $Task->getDateAdded(), $dashboardManager->GetIdDashBoardByIdTask($Task->GetId()) ]);
+            $Task->setId($this->getLastInsertID());
+            return $Task;
+        } catch (PDOException $e) {
+            // In case of an error, redirect to the error page with a message
+            $errorMessage = "An error occurred while adding the Task.";
+            header("Location: index.php?action=Index&errorMessage=".urlencode($errorMessage));
+            exit();
+        }
     }
  }
