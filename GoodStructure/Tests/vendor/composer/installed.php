6fdfd0af9034804bf58ac251e4f2edda9cf82767
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-11-20 15:59:56 +0100

    Update installed.php

--- GoodStructure/Tests/vendor/composer/installed.php
+++ GoodStructure/Tests/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b4e7351fad88f08bd785f1026f9c59a4483e6289',
+        'reference' => 'b00aa96ec3ec719148078ff7d7a0451101808947',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b4e7351fad88f08bd785f1026f9c59a4483e6289',
+            'reference' => 'b00aa96ec3ec719148078ff7d7a0451101808947',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
