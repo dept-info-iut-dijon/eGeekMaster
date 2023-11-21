0a44a5413ba1c7f0e47aebfdb4958a5f0015d4de
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-21 15:56:43 +0100

    TestLoginController.php testAdd

---
+++ GoodStructure/Tests/TestLoginController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Nicolas
+ */
+require 'vendor\autoload.php';
+use PHPUnit\Framework\TestCase;
+
+
+class TestLoginController extends TestCase
+{
+
+    public function testAdd()
+    {
+
+        $this->assertSame(1,1);
+        
+    }
+
+    
+}
+?>
