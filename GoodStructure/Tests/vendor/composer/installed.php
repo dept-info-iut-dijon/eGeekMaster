92393e5fb61496f8298d9a58bc216b8b8106ae36
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-21 13:30:13 +0100

    composer update

--- GoodStructure/Tests/vendor/composer/installed.php
+++ GoodStructure/Tests/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '299277bcf52aa2de0c7cff771bd6f87047af7fc7',
+        'name' => 'egeekmaster/testunitaire',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '299277bcf52aa2de0c7cff771bd6f87047af7fc7',
+        'egeekmaster/testunitaire' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
