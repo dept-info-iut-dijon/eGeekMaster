66eb8029f1e591218929ac920161ca826592c316
Lola Cohidon <116801009+LoLuX22@users.noreply.github.com>
2023-11-21 13:47:06 +0100

    FollowUp.php/Header

--- GoodStructure/Views/viewFollowUp.php
+++ GoodStructure/Views/viewFollowUp.php
@@ -1,11 +1,19 @@
 <!-- autor : Lola Cohidon -->
 <link rel="stylesheet" href="">
 <div id = "viewFollowUp">
-    <!--Affichage nom et prénon de l'utilisateur-->
 
     <!-- Header -->
-    <div id="header">
-        <h1>Nom Prénom</h1>
+    <div id="Person">
+        <h1>Nom Prénom</h1> <!--Displaying the user's first and last name-->
     </div>
+    <!--Buttons-->
+    <div class="buttons">
+        <button onclick="showDetailed()">Suivi détaillé</button>
+        <button onclick="showGlobal()">Suivi global</button>
+    </div>
+    <!--Content-->
+    <div class = "content" id = "content">
+</div>
 
+<script src="Public/Animation_js/followUp.js"></script>
     
