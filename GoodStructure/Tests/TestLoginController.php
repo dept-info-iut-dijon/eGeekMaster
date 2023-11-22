d7a61387efa09927b4ffddd781393bc5ae10cc70
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-22 11:29:45 +0100

    TestLoginController.php testAdd

--- GoodStructure/Tests/TestLoginController.php
+++ GoodStructure/Tests/TestLoginController.php
@@ -3,6 +3,7 @@
  * @author Nicolas
  */
 require 'vendor\autoload.php';
+require 'Controllers\LoginController.php';
 use PHPUnit\Framework\TestCase;
 
 
@@ -11,6 +12,30 @@ class TestLoginController extends TestCase
 
     public function testAdd()
     {
+        $LoginManager = new LoginManager();
+        $LoginController = new LoginController();
+        $testLogin = new Login("coco2","12345");
+        $testLogin->setPassword("12345");
+
+        $_POST = [
+            'Username' => $testLogin->getLogin(),
+            'Password' => $testLogin->getPassword()
+        ];
+        
+        $LoginController->Add();
+
+        $listeLogin = $LoginManager->getAll();
+
+        // Rechercher l'utilisateur correspondant
+        $pullLogin = null;
+        foreach($listeLogin as $login){
+            if($login->attributesEquals($testLogin)){
+                $pullLogin = $login;
+            }
+        }
+
+        // Assertion
+        $this->assertNotNull($pullLogin, 'Le login devrait être ajouté avec succès');
 
         $this->assertSame(1,1);
         
