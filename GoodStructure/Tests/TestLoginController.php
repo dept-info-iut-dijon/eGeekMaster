ddb33eedf533d564b7f33a6be42a884847bd8afc
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-22 15:06:07 +0100

    TestLoginController.php testDelete, TestConnect

--- GoodStructure/Tests/TestLoginController.php
+++ GoodStructure/Tests/TestLoginController.php
@@ -9,7 +9,10 @@ use PHPUnit\Framework\TestCase;
 
 class TestLoginController extends TestCase
 {
-
+    /**
+     * Test de la méthode Add de la classe LoginController
+     * @author Nicolas
+     */
     public function testAdd()
     {
         // Création d'instance
@@ -51,6 +54,10 @@ class TestLoginController extends TestCase
         $LoginController->Delete();
     }
 
+    /**
+     * Test de la méthode Delete de la classe LoginController
+     * @author Nicolas
+     */
     public function testDelete()
     {
         // Création d'instance
@@ -104,11 +111,56 @@ class TestLoginController extends TestCase
                 $pullLogin2 = $log;
             }
         }
-        var_dump($pullLogin);
-        var_dump($pullLogin2);
-        var_dump($_POST['id']);
+
         // Assertion pour tester si le login a bien été supprimé
         $this->assertNull($pullLogin2, 'Le login devrait être supprimé avec succès');
+    }
+
+    /**
+     * Test de la méthode Connect de la classe LoginController
+     * @author Nicolas
+     */
+    public function testConnect()
+    {
+        // Création d'instance
+        $LoginManager = new LoginManager();
+        $LoginController = new LoginController();
+
+        // Variable de test
+        $testLogin = new Login("coco2","12345");
+        $testLogin->setPassword("12345");
+
+        $_POST = [
+            'Username' => $testLogin->getLogin(),
+            'Password' => $testLogin->getPassword()
+        ];
+
+        // Appeler la méthode pour l'ajout d'un login à connecter
+        $LoginController->Add();
+
+        // Récupérer la liste des utilisateurs après l'ajout
+        $listeLogin = $LoginManager->getAll();
+
+        // Rechercher du login correspondant pour récuperer son id
+        foreach($listeLogin as $login){
+            $log = new Login($login['username'],$login['Hash']);
+            $log->setId($login['idLogin']);
+            if($log->attributesEquals($testLogin)){
+                $testLogin->setId($log->getId());
+            }
+        }
+        $_POST['Password'] = 12345;
+        $_POST['id'] = $testLogin->getId();
+
+        // Execution de la méthode de connexion a tester
+        $LoginController->Connect();
+
+        // Récupération de la session
+        $this->assertSame($_SESSION['IdLogin'], $testLogin->getId(), 'La connexion devrait être réussie');
+
+        // Suppression du login ajouté pour le test
+        $_POST['id'] = $testLogin->getId();
+        $LoginController->Delete();
     }
 
     
