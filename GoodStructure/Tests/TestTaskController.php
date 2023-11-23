54823b3c0940ebeb13d2dde2721493a432089349
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-23 12:27:19 +0100

    TestTaskController.php

--- GoodStructure/Tests/TestTaskController.php
+++ GoodStructure/Tests/TestTaskController.php
@@ -73,7 +73,52 @@ class TestTaskController extends TestCase
 
    public function testAddTaskUpdate()
    {
-    
+      // Création d'instance
+      $Task = new Task(156,"Shopping",1,"2021-05-03",44);
+      $Task2 = new Task(156,"Cleaning",8,"2011-07-03",44);
+      $TaskManager = new TaskManager();
+      $TaskController = new TaskController();
+
+      // Données de test
+      $_SESSION['IdLogin'] = 86;
+
+      $_POST = [ 
+         'searchTask' => 'Shopping',
+         'hours' => '0',
+         'minutes' => '15',
+         'Date' => '2021-05-03'
+      ];
+
+      $_SERVER["REQUEST_METHOD"] = "POST";
+
+      // Appeler la méthode à tester pour l'ajout d'une tâche pour la modifier ensuite
+      $TaskController->AddTask();
+
+      // Vérifier que la tâche 1 a bien été ajoutée
+      $this->assertTrue($TaskManager->CheckIfTaskExists($Task));
+      // Verifier que la tâche 2 n'existe pas
+      $this->assertFalse($TaskManager->CheckIfTaskExists($Task2));
+
+      // Données de test
+      $_GET['action'] = "TaskModification";
+
+      $_POST = [ 
+         'searchTask' => 'Cleaning',
+         'hours' => '2',
+         'minutes' => '0',
+         'Date' => '2011-07-03'
+      ];
+
+      $TaskController->AddTask();
+
+      // Vérifier que la tâche 2 a bien été ajoutée
+      $this->assertTrue($TaskManager->CheckIfTaskExists($Task2));
+      // Verifier que la tâche 1 n'existe plus
+      $this->assertFalse($TaskManager->CheckIfTaskExists($Task));
+
+
+      // Suppression de la tâche ajoutée pour le test
+      $TaskController->DeleteTask();
    }
 
     
