72860d7f10ccd372bf4fd48eeb3009b008cc669a
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2023-11-23 16:33:36 +0100

    MyHomeManager.php : creation MyHomeManager.php

---
+++ GoodStructure/Models/MyHomeManager.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Class MyHomeManager
+     * @author Enzo
+     */
+    class MyHomeManager extends Model {
+        public function __construct() {
+            parent::__construct();
+        }
+
+        
+    }
+?>
