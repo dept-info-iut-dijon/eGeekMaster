f85bbe54cd050817eec9c17494e02e0e1a5a2f2f
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-11-23 01:10:09 +0100

    TestDashboardController.php testDelete, testUpdateDashboard

--- GoodStructure/Tests/testDashboardController.php
+++ GoodStructure/Tests/testDashboardController.php
@@ -1,31 +1,19 @@
 <?php
+/**
+ * @author Nicolas
+ */
 use PHPUnit\Framework\TestCase;
 
-class testDashboardController extends TestCase{
-    public function testDelete() {
-        $DashBoardManager = $this->createMock(DashBoardManager::class);
-        $MainController = $this->createMock(MainController::class);
-        $dashBoardController = new DashBoardController($DashBoardManager, $MainController);
-        $_POST["idDashBoard"] = 100;
-        $DashBoardManager->expects($this->once())
-                             ->method('DeleteByID')
-                             ->with(100);
-        $MainController->expects($this->once())
-                           ->method('Index')
-                           ->with($this->equalTo("DashBoard supprimé"));
-        $dashBoardController->Delete();
+class TestDashboardController extends TestCase{
+
+    public function testDelete()
+    {
+
     }
 
-    public function testUpdateDashBoard() {
-        $dashBoardController = new DashBoardController();
-        $dashBoard = new DashBoard();
-        $dashBoard->SetId(1);
-        $dashBoard->SetUsername('JohnDoe');
-        $dashBoard->SetIdUser(123);
-        $reflection = new ReflectionClass('DashBoardController');
-        $method = $reflection->getMethod('UpdateDashBoard');
-        $method->setAccessible(true);
-        $method->invoke($dashBoardController, $dashBoard);
+    public function testUpdateDashBoard()
+    {
+
     }
 }
 ?>
