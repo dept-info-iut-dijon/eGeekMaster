b58249313087847b0263107b4e987855a01bc8a9
Nicolas Desertot <nicolas.desertot@orange.fr>
2023-11-23 01:08:53 +0100

    TestTaskController.php testAddTask, testDeleteTask, testUpdateTask

---
+++ GoodStructure/Tests/TestTaskController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Nicolas
+ */
+require 'vendor\autoload.php';
+require 'Controllers\LoginController.php';
+use PHPUnit\Framework\TestCase;
+
+
+class TestTaskController extends TestCase
+{
+   public function testAddTask()
+   {
+    
+   }
+
+   public function testDeleteTask()
+   {
+    
+   }
+
+   public function testUpdateTask()
+   {
+    
+   }
+
+    
+}
+?>
