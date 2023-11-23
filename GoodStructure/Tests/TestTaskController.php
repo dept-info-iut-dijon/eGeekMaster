e6d1418cb131bfcc502f38885b35f044d0806343
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-11-23 11:34:12 +0100

    TestTaskController.php testAddTaskCreate, testDeleteTask

--- GoodStructure/Tests/TestTaskController.php
+++ GoodStructure/Tests/TestTaskController.php
@@ -4,6 +4,7 @@
  */
 require 'vendor\autoload.php';
 require 'Controllers\LoginController.php';
+require 'Controllers\TaskController.php';
 use PHPUnit\Framework\TestCase;
 
 
@@ -11,12 +12,63 @@ class TestTaskController extends TestCase
 {
    public function testAddTaskCreate()
    {
-      
+      // Création d'instance
+      $Task = new Task(156,"Shopping",1,"2021-05-03",44);
+      $TaskManager = new TaskManager();
+      $TaskController = new TaskController();
+
+      // Données de test
+      $_SESSION['IdLogin'] = 86;
+
+      $_POST = [ 
+         'searchTask' => 'Shopping',
+         'hours' => '0',
+         'minutes' => '15',
+         'Date' => '2021-05-03'
+      ];
+
+      $_SERVER["REQUEST_METHOD"] = "POST";
+
+      // Appeler la méthode à tester pour l'ajout d'une tâche
+      $TaskController->AddTask();
+
+      // Vérifier que la tâche a bien été ajoutée
+      $this->assertTrue($TaskManager->CheckIfTaskExists($Task));
+
+      // Suppression de la tâche ajoutée pour le test
+      $TaskController->DeleteTask();
    }
 
    public function testDeleteTask()
    {
-    
+      // Création d'instance
+      $Task = new Task(156,"Shopping",1,"2021-05-03",44);
+      $TaskManager = new TaskManager();
+      $TaskController = new TaskController();
+
+      // Données de test
+      $_SESSION['IdLogin'] = 86;
+
+      $_POST = [ 
+         'searchTask' => 'Shopping',
+         'hours' => '0',
+         'minutes' => '15',
+         'Date' => '2021-05-03'
+      ];
+
+      $_SERVER["REQUEST_METHOD"] = "POST";
+
+      // Appeler la méthode à tester pour l'ajout d'une tâche
+      $TaskController->AddTask();
+
+      // Vérifier que la tâche a bien été ajoutée
+      $this->assertTrue($TaskManager->CheckIfTaskExists($Task));
+
+      // Suppression de la tâche ajoutée pour le test
+      $TaskController->DeleteTask();
+
+      // Vérifier que la tâche a bien été supprimée
+      $this->assertFalse($TaskManager->CheckIfTaskExists($Task)); 
    }
 
    public function testAddTaskUpdate()
