28f20afe6aa5ce47a1053513a851633b25ad45d9
EnzoPigeron <134093904+EnzoPigeron@users.noreply.github.com>
2023-11-24 11:30:23 +0100

    MyHomeManager.php / UpdateMyHome.php

--- GoodStructure/Models/MyHomeManager.php
+++ GoodStructure/Models/MyHomeManager.php
@@ -1,4 +1,7 @@
 <?php
+    require_once 'Model.php';
+    require_once 'MyHome.php';
+
     /**
      * Class MyHomeManager
      * @author Enzo
@@ -16,15 +19,37 @@
          */
         public function AddMyHome(MyHome $myHome) : MyHome {
             try {
-                $sql = 'INSERT INTO myhome (codeMyHome, usernameMyHome) VALUES (:value1, :value2)';
+                $sql = 'INSERT INTO myhome (codeMyHome, nameMyHome) VALUES (:value1, :value2)';
                 $this->executerRequete($sql, [
                     ':value1'=> $myHome->GetCodeMyHome(),
-                    ':value2'=> $myHome->GetUsernameMyHome()
+                    ':value2'=> $myHome->GetNameMyHome()
                 ]);
                 return $myHome;
             } catch (Exception $e) {
                 // if error, we are redirect to the index page with an error
                 $errorMessage = "An error occured while adding a home";
+                header("Location : index.php?action=MyHome&errorMessage".urlencode($errorMessage));
+                exit();
+            }
+        }
+
+        /**
+         * Update a home in the database
+         * @author Enzo
+         * @param $myHome the myHome object to update
+         * @return MyHome return a myHome object
+         */
+        public function UpdateMyHome(MyHome $myHome) : MyHome {
+            try {
+                $sql = "UPDATE myhome SET nameMyHome = :value1 WHERE idMyHome = :value2";
+                $this->executerRequete($sql, [
+                    ":value1"=> $myHome->GetNameMyHome(),
+                    ":value2"=> $myHome->GetIdMyHome()
+                ]);
+                return $myHome;
+            } catch (Exception $e) {
+                // if error, we are redirect to the index page with an error
+                $errorMessage = "An error occured while updating a home";
                 header("Location : index.php?action=MyHome&errorMessage".urlencode($errorMessage));
                 exit();
             }
