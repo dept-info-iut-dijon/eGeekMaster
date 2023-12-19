478e4006894e038ac1dbaf4f8acd59a534943a07
TheoDeguin <105062188+TheoDeguin@users.noreply.github.com>
2023-12-19 18:38:50 +0100

    Companion
    référencement

--- GoodStructure/Controllers/MainController.php
+++ GoodStructure/Controllers/MainController.php
@@ -124,113 +124,7 @@ class MainController {
     
     }
 
-    /**
-     * Displays the Follow up page
-     * @author Enzo
-     * @param $message 
-     * @param $idLastTast
-     * @param $nameLastTask
-     * @param $durationLastTask
-     * @param $dateLastTask 
-    */
-    public function FollowUp($message = null, $idLastTast = null, $nameLastTask = null, $durationLastTask = null, $dateLastTask = null) {
     
-    $taskDurations = [];
-    $labels = [];
-    $tempT = 0;
-    $taskDates = [];
-    $taskCountPerYearMonth = [];
-    $taskCountPerYear = [];
-
-    if (isset($_SESSION['tasks'])) {                         
-        foreach ($_SESSION['tasks'] as $task) {
-            // Get name, duration and date of the task
-            $taskName = $task->getNameTask();
-            $taskDuration = $task->getDuration();
-            $taskDate = $task->getDateAdded();
-
-            // Separate the date in year and month
-            $year = date('Y', strtotime($taskDate));
-            $month = date('n', strtotime($taskDate));
-
-            // If taskname is not in labels
-            if (!in_array($taskName, $labels)) {
-                // For detail part
-                $labels[] = $taskName;
-                $taskDurations[$taskName] = $taskDuration;
-                $tempT += $taskDuration;
-
-                // For global part
-                // Per year
-                if (!isset($taskCountPerYear[$year][$taskName])) {
-                    $taskCountPerYear[$year][$taskName] = 1;
-                } else {
-                    $taskCountPerYear[$year][$taskName]++;
-                }
-                // Per month
-                if (!isset($taskCountPerYearMonth[$year][$month][$taskName])) {
-                    $taskCountPerYearMonth[$year][$month][$taskName] = 1;
-                } else {
-                    $taskCountPerYearMonth[$year][$month][$taskName]++;
-                }
-            } else {
-                // For detail part
-                $taskDurations[$taskName] += $taskDuration;
-                $tempT += $taskDuration;
-
-                // For global part
-                // Per year
-                if (!isset($taskCountPerYear[$year][$taskName])) {
-                    $taskCountPerYear[$year][$taskName] = 1;
-                } else {
-                    $taskCountPerYear[$year][$taskName]++;
-                }
-                // Per month
-                if (!isset($taskCountPerYearMonth[$year][$month][$taskName])) {
-                    $taskCountPerYearMonth[$year][$month][$taskName] = 1;
-                } else {
-                    $taskCountPerYearMonth[$year][$month][$taskName]++;
-                }
-            }
-        }
-
-        foreach ($labels as $label) {
-            // For detail part
-            $taskPercent[$label] = ceil(($taskDurations[$label]*100) / $tempT);      
-            
-        }
-    }
-
-
-
-
-        // Separate the duration into hours and minutes
-        $durationLastTaskminutes = strval($durationLastTask*15%60);
-        $durationLastTaskhours = strval($durationLastTask*15%4);
-
-        // Create a view Follow up
-        $referenceView = new View("FollowUp");
-        // Data to generate the page
-        $data = array(
-            //ajouter les données à afficher
-        );
-        if($message != null) {
-            $data = $message;
-        }
-        $data["idLastTast"] = $idLastTast;
-        $data["nameLastTask"] = $nameLastTask;
-        $data["durationLastTaskhours"] = $durationLastTaskhours;
-        $data["durationLastTaskminutes"] = $durationLastTaskminutes;
-        $data["dateLastTask"] = $dateLastTask;
-        $data["taskPercent"] = $taskPercent;
-        $data["taskDurations"] = $taskDurations;
-        $data["taskCountPerYearMonth"] = $taskCountPerYearMonth;
-        $data["taskCountPerYear"] = $taskCountPerYear;
-        $data["labels"] = $labels;
-
-        // Generate the view
-        $referenceView->generer($data);
-    }
 
     /**
      * Displays the Follow up page
@@ -319,9 +213,7 @@ class MainController {
         // Create a view Follow up
         $referenceView = new View("FollowUp");
         // Data to generate the page
-        $data = array(
-            //ajouter les données à afficher
-        );
+        $data = $this->initializeIndexData();
         if($message != null) {
             $data = $message;
         }
