5310a6cd468d6374efa9d5071aa0093019800f70
Nicolas Desertot <105040325+NicolasDesertot@users.noreply.github.com>
2023-12-20 15:18:09 +0100

    update traduction viewCookiePolicy.php

--- GoodStructure/Views/viewCookiePolicy.php
+++ GoodStructure/Views/viewCookiePolicy.php
@@ -1,38 +1,38 @@
 <link rel="stylesheet" href="Public/css/mentionLegal.css"/>
 <div class="mentionLegal">
-    <h1>Cookie Policy for Famil'Easy</h1>
-    <h2>Introduction :</h2>
+    <h1><?= $translations[$language]['cookiePolicy_title']?></h1>
+    <h2><?= $translations[$language]['cookiePolicy_intro']?></h2>
     <p>
-        We use cookies to improve your experience on our site, www.famileasy.com, operated by Famil'Easy. This policy explains how we use cookies and the choices you have.
+        <?= $translations[$language]['cookiePolicy_desc']?> 
     </p>
-    <h2>What are Cookies ?</h2>
+    <h2><?= $translations[$language]['cookiePolicy_1_title']?></h2>
     <p>
-        Cookies are small text files that are placed on your device by websites that you visit. They are widely used to make websites work, or work more efficiently, as well as to provide information to the site owners.
+        <?= $translations[$language]['cookiePolicy_1_desc']?>
     </p>
-    <h2>Types of Cookies We Use :</h2>
+    <h2><?= $translations[$language]['cookiePolicy_2_title']?></h2>
     <p>
-        Essential Cookies: These are necessary for the operation of our site. They include, for example, cookies that enable you to log into secure areas of our website.
-    </p>
-    <p>
-        Analytical/Performance Cookies: They allow us to recognize and count the number of visitors and to see how visitors move around our site when they are using it.
+        <?= $translations[$language]['cookiePolicy_2_desc1']?>
     </p>
     <p>
-        Functionality Cookies: These are used to recognize you when you return to our site. This enables us to personalize our content for you and remember your preferences.
+        <?= $translations[$language]['cookiePolicy_2_desc2']?>
     </p>
-    <h2>Third-Party Cookies:</h2>
     <p>
-        Our site may use third-party services that also set cookies on your device when you visit our website. These cookies are managed by the respective third parties, and you should refer to their own privacy policies.
+        <?= $translations[$language]['cookiePolicy_2_desc3']?>
     </p>
-    <h2>Consent and Control:</h2>
+    <h2><?= $translations[$language]['cookiePolicy_3_title']?></h2>
     <p>
-        Upon visiting our site, you will be prompted to accept or decline the use of cookies. You can also modify your cookie preferences in your browser settings.
+        <?= $translations[$language]['cookiePolicy_3_desc']?>
     </p>
-    <h2>Changes to Our Cookie Policy:</h2>
+    <h2><?= $translations[$language]['cookiePolicy_4_title']?></h2>
     <p>
-        We may update this cookie policy from time to time. Any changes will be posted on this page.
+        <?= $translations[$language]['cookiePolicy_4_desc']?>
     </p>
-    <h2>Contact Information:</h2>
+    <h2><?= $translations[$language]['cookiePolicy_5_title']?></h2>
     <p>
-        If you have any questions about our use of cookies, please contact us at contact@famileasy.com.
+        <?= $translations[$language]['cookiePolicy_5_desc']?>
+    </p>
+    <h2><?= $translations[$language]['cookiePolicy_6_title']?></h2>
+    <p>
+        <?= $translations[$language]['cookiePolicy_6_desc']?>
     </p>
 </div>
